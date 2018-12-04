1571a0a8d36261a6fdd27b9e36127432cd966176
dordovel <sernfst58@mail.ru>
2018-12-04 20:58:24 +0300

    1

--- C/Controller.cpp
+++ C/Controller.cpp
@@ -63,36 +63,38 @@ void Controller::listenning() {
     std::string temp;
     bool flag=false;
 
-    while (true) {
-        if (!this->view->getOpenWindowFlag())return;
+    while (true)
+    {
+        if (!this->view->getOpenWindowFlag())break;
 
-        if ((flag = this->view->getFlagEventShow())) {
-
+        if ((flag = this->view->getFlagEventShow()))
+        {
             this->view->invertFlagEventShow();
         }
 
-        this->file->write_message();
+        if(this->file->write_message())
+        {
+            temp = this->file->getResponse();
 
-        temp = this->file->getResponse();
-
-        if (temp == "request") {
-
-            if (!flag)
+            if (temp == "request")
             {
-                this->file->send_message((char *) "200");
-            }
-            else {
-                this->file->send_message(this->view->getMessage());
-            }
-        }
-        else
-            {
-            this->view->show((char *) temp.c_str());
-        }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(400));
-        flag = false;
+                if (!flag)
+                {
+                    this->file->send_message((char *) "200");
+                }
+                else
+                    {
+                    this->file->send_message(this->view->getMessage());
+                }
+            } else
 
+                {
+                this->view->show((char *) temp.c_str());
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(400));
+        }
     }
 }
 
